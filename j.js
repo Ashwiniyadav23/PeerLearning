6bcf5ccaf0f0b9c46b788cfc6126e0523888be5c
ashwini <ashwiniyadav23@navgurukul.org>
2025-01-06 11:02:40 +0530

    i have written diffrent code

---
+++ j.js
@@ -0,0 +1,8 @@
+// const div = document.querySelector("div");
+//  div.setAttribute('id',"container");
+ const button = document.getElementById("btn");
+ console.log(button)
+ button.addEventListener('click',() =>{
+    document.body.classList.toggle("Dark");
+   
+ })
