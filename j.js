8ee8e75f3c71d2d41f1a711304620c5d2604889c
ashwini <ashwiniyadav23@navgurukul.org>
2025-01-06 11:30:53 +0530

    add this code

--- j.js
+++ j.js
@@ -1,8 +1,15 @@
 // const div = document.querySelector("div");
 //  div.setAttribute('id',"container");
- const button = document.getElementById("btn");
- console.log(button)
- button.addEventListener('click',() =>{
-    document.body.classList.toggle("Dark");
+//  const button = document.getElementById("btn");
+//  console.log(button)
+//  button.addEventListener('click',() =>{
+//     document.body.classList.toggle("Dark");
    
- })
+//  })
+
+
+
+fetch("https://api.sheetbest.com/sheets/66931299-448d-4d0c-b535-7eeac70e1c3d")
+.then(response => response.json())
+.then(data => console.log(data))
+.catch(error => console.error("error",error));
