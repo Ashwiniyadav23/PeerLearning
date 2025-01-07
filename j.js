e88d29f405366cf33d5a1c978040fe7dbc29a9bf
ashwini <ashwiniyadav23@navgurukul.org>
2025-01-07 11:39:01 +0530

    bootsrap code

--- j.js
+++ j.js
@@ -23,7 +23,7 @@
 //       'Content-Type': 'application/json'
 //     },
 //     body: JSON.stringify({
-//       Name: "Ashwinui",
+//       Name: "Ashwini",
 //       Email: "ashwiniyadav23@navgurukul.org",
 //       Age: 18,
 //       Class: 12
@@ -67,25 +67,26 @@
 // .then(data => console.log(data))
 // .catch(error => console.error("error",error));
 
-fetch('https://api.sheetbest.com/sheets/66931299-448d-4d0c-b535-7eeac70e1c3d', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      name: "Prerna",
-      Email: "prernasai23@navgurukul.org",
-      Age: 18,
-      Class: 12
-    })
-  })
-    .then(response => {
-      if (!response.ok) {
-        console.error('Error:', response.statusText);
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      return response.json();
-    })
-    .then(data => console.log('Success:', data))
-    .catch(error => console.error('Fetch Error:', error));
+// fetch('https://api.sheetbest.com/sheets/66931299-448d-4d0c-b535-7eeac70e1c3d', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({
+//       name: "Prerna",
+//       Email: "prernasai23@navgurukul.org",
+//       Age: 18,
+//       Class: 12
+//     })
+//   })
+//     .then(response => {
+//       if (!response.ok) {
+//         console.error('Error:', response.statusText);
+//         throw new Error(`HTTP error! status: ${response.status}`);
+//       }
+//       return response.json();
+//     })
+//     .then(data => console.log('Success:', data))
+//     .catch(error => console.error('Fetch Error:', error));
   
+
